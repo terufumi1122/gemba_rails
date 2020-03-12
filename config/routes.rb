a94502db44f9ddb78f6a8ae4ed61aabcc12f5af7
terufumi1122 <terufumi1122@gmail.com>
2020-03-12 18:12:27 +0900

    vies: ユーザー詳細画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :users, only: [:index, :new, :edit, :show]
+    resources :users
   end
   root to: 'tasks#index'
   resources :tasks
