66306779a2b64ffcddc0b84ce0c4684b972591b7
terufumi1122 <terufumi1122@gmail.com>
2020-03-16 17:51:18 +0900

    db: seedデータをfind_or_create_byメソッドで書き換え

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create!(
-  name:                  'admin',
-  email:                 'admin@example.com',
-  admin:                 true,
-  password:              'password',
-  password_confirmation: 'password'
-)
+User.find_or_create_by!(email: 'admin@example.com') do |user|
+  user.name =                  'admin',
+  user.email =                 'admin@example.com',
+  user.admin =                 true,
+  user.password =              'password',
+  user.password_confirmation = 'password'
+end
